131bc9c081bb495bae60133e9cfeaf5b2d27c856
ghuleaniketh <ghuleaniketh@gmail.com>
2026-09-08 04:59:38 +0530

    fix(cors): add hackshastra-web.vercel.app to allowedOrigins

--- src/app.js
+++ src/app.js
@@ -34,6 +34,7 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://127.0.0.1:5173',
   'https://hackshastrasrmuap.dev',
+  'https://hackshastra-web.vercel.app',
   env.CLIENT_URL,
 ].filter(Boolean);
 
